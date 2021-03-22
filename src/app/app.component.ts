09f35313fae8c0101f80b9a9f51456160648d1e3
unknown <yvonnegamboeck@web.de>
2021-03-22 10:58:36 +0100

    added images and startet to work on slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'yvonnegamboeck';
+export class AppComponent implements OnInit {
 
+images = ['erfolgreiche-junge-frau-im-modernen-buero-das-an-laptop-arbeitet_109710-1521.jpg', 'OVEDMK0.jpg', 'landingpage-vorlage-fuer-den-weltgrafiktag_23-2148891523.jpg'];
+currentImage = 0;
+showImage = true; 
 
+/**
+ * This function inits when the app has loaded
+ */
+ngOnInit() {
+  this.updateImage();
+}
 
+updateImage() {
 
-  // Hier kommt der JS Tag rein
+setInterval(() => {
+this.currentImage++;
+this.currentImage = this.currentImage % this.images.length; //rechnet den "Rest" aus, damit nach 3 Bildern wieder von vorne losgeht
+this.showImage = false;
+
+setTimeout(() => {
+this.showImage = true; 
+}, 10)
+}, 8000);
+}
+
 }
